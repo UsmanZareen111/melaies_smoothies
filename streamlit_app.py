e698275b9ec153eae98132a13dc13c5ae9f373f5
UsmanZareen111 <usmanzareen16@gmail.com>
2025-12-29 02:00:09 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,41 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+import requests
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie!")
+st.write("Choose the fruits you want in your custom Smoothie!")
+
+name_on_order = st.text_input("Name on smoothie:")
+st.write("The name on your smoothie will be:", name_on_order)
+
+cnx = st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe,
+    max_selections=5
+)
+
 if ingredients_list:
     ingredients_string = ''
-
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
-        # This subheader and dataframe will show for each fruit selected
         st.subheader(fruit_chosen + ' Nutrition Information')
-        
-        # Using fruit_chosen makes the API call dynamic for each selection
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    # These lines must be aligned with the "for" loop to run after the loop finishes
+    # Prepare the SQL insert statement
     my_insert_stmt = (
         "INSERT INTO smoothies.public.ORDERS (ingredients, NAME_ON_ORDER) "
         "VALUES ('" + ingredients_string + "', '" + name_on_order + "')"
     )
 
-    time_to_insert = st.button('Submit order')
+    time_to_order = st.button('Submit order')
     
-    if time_to_insert:
+    if time_to_order:
         session.sql(my_insert_stmt).collect()
-        st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# sf = st.dataframe(data =smoothiefroot_response.json(),use_container_width=True)
+        st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
